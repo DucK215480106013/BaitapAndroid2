a2fdd0a355557e2a1f885a164a62132b17dc0977
DucK215480106013 <k215480106013@tnut.edu.vn>
2025-02-15 20:37:41 +0700

    lỗi sdk

--- app/src/main/java/com/example/bai2/ChiTietActivity.java
+++ app/src/main/java/com/example/bai2/ChiTietActivity.java
@@ -1,6 +1,8 @@
 package com.example.bai2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ChiTietActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,21 @@ public class ChiTietActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView tvId = findViewById(R.id.tv_id_detail);
+        TextView tvUserID = findViewById(R.id.tv_userID_detail);
+        TextView tvTomtat = findViewById(R.id.tv_tomtat_detail);
+        TextView tvFullbody = findViewById(R.id.tv_fullbody_detail);
+
+        // Get data from intent
+        int id = getIntent().getIntExtra("id",0);
+        int userID = getIntent().getIntExtra("userID",0);
+        String tomtat = getIntent().getStringExtra("tomtat");
+        String fullbody = getIntent().getStringExtra("fullbody");
+
+        // Set data
+        tvId.setText("ID: " + id);
+        tvUserID.setText("User ID: " + userID);
+        tvTomtat.setText("Tóm tắt: " + tomtat);
+        tvFullbody.setText("Nội dung đầy đủ: " + fullbody);
     }
 }
--- app/src/main/java/com/example/bai2/MainActivity.java
+++ app/src/main/java/com/example/bai2/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.bai2;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    MessageAdapter adapter;
+
+    List<MessageModel> messageList; //bien toan cuc
+    AppCompatButton button;
+    TextView id,iduser,title,message, txt_dem;
+    Gson gson;
+    MessageModel messageModule = new MessageModel();
+    private static final String CHANNEL_ID = "my_channel_id";
+    String json;
+    int dem=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +55,110 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        anhXa();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                messageList = new ArrayList<>();
+
+                // Tạo Retrofit instance
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://jsonplaceholder.typicode.com/posts/") // URL chính xác của API
+                        .addConverterFactory(GsonConverterFactory.create()) // Dùng Gson để parse JSON
+                        .build();
+
+                Api apiService = retrofit.create(Api.class);
+                Call<List<MessageModel>> call = apiService.getJsonData(); // Sửa Call<List<MessageModule>>
+
+                call.enqueue(new Callback<List<MessageModel>>() {
+                    @Override
+                    public void onResponse(Call<List<MessageModel>> call, Response<List<MessageModel>> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            messageList = response.body();
+                            dem=0;
+                            if (!messageList.isEmpty()) {
+                                HienThi1PT();
+                                MessageAdapter adapter;
+                                adapter = new MessageAdapter(getApplicationContext(), messageList);
+                                recyclerView.setAdapter(adapter);
+                                MessageModel messageModel = messageList.get(dem); // Lấy bài viết đầu tiên
+                                id.setText(String.valueOf(messageModel.getId()));
+                                iduser.setText(String.valueOf(messageModel.getUserId()));
+                                title.setText(messageModel.getTitle());
+                                message.setText(messageModel.getBody());
+                                dem++;
+                                showNotification(messageModel.getTitle(),messageModel.getBody());
+                            }
+                        } else {
+                            System.err.println("Response is empty or unsuccessful");
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<List<MessageModel>> call, Throwable t) {
+                        System.err.println("Error: " + t.getMessage());
+                    }
+                });
+            }
+        });
+    }
+
+    private void anhXa() {
+        button = findViewById(R.id.button);
+        id = findViewById(R.id.id1);
+        title = findViewById(R.id.title1);
+        message = findViewById(R.id.message1);
+        iduser = findViewById(R.id.iduser1);
+        txt_dem = findViewById(R.id.txt_dem);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+    void HienThi1PT(){
+        MessageModel messageModule = messageList.get(dem); // Lấy bài viết đầu tiên
+        id.setText(String.valueOf(messageModule.getId()));
+        iduser.setText(String.valueOf(messageModule.getUserId()));
+        title.setText(messageModule.getTitle());
+        message.setText(messageModule.getBody());
+        txt_dem.setText(""+(dem+1)+"/"+messageList.size());
+        showNotification(messageModule.getTitle(),messageModule.getBody());
+    }
+    private void showNotification(String title, String content) {
+        // Tạo NotificationManager
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Tạo NotificationChannel (chỉ cần với Android 8.0+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Tên kênh thông báo",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription("Mô tả kênh thông báo");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE // Yêu cầu để chạy trên Android 12+
+        );
+
+        // Tạo Notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background) // Icon nhỏ cho thông báo (thêm icon vào res/drawable)
+                .setContentTitle(title)                   // Tiêu đề thông báo
+                .setContentText(content)                 // Nội dung thông báo
+                .setContentIntent(pendingIntent) //bam vao thi mo activity nao
+                .setAutoCancel(true)         //bam vao thi close Thong bao
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT); // Đặt mức ưu tiên
+
+        // Hiển thị thông báo
+        notificationManager.notify(1, builder.build());
     }
 }
